53835878f144ccafc942ba44872513eb84652fd2
Alex Baklanov <alexandr.baklanov@gmail.com>
2019-02-22 12:21:20 +0300

    Add the first simplest test

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Dog guessr</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
+// render(<App />, document.getElementById("app"));
