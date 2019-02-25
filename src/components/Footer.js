160a170a9e5ed68f70a7f2c9472bd96c9c4fc55b
Alex Baklanov <alexandr.baklanov@gmail.com>
2019-02-25 13:13:00 +0300

    Add Skip button functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 class Footer extends React.Component {
+  handleSkipButtonClick() {
+    // Just reload the page
+    document.location.replace("/");
+  }
+
   render() {
     return (
       <div className="row">
-        <Button variant="secondary" id="skip">
+        <Button
+          variant="secondary"
+          id="skip"
+          onClick={this.handleSkipButtonClick}
+        >
           Skip
         </Button>
       </div>
--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -13,3 +13,11 @@ it("displays Skip button", () => {
   const button = wrapper.find(Button).find("#skip");
   expect(button).toHaveLength(1);
 });
+
+it("redirects when Skip button is clicked", () => {
+  window.location.replace = jest.fn();
+
+  const button = wrapper.find(Button).find("#skip");
+  button.simulate("click");
+  expect(window.location.replace).toHaveBeenCalled();
+});
